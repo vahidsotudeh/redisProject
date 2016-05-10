6ef33ac0d21acf428c79f3bd13c4be3f3b8a35b2
Microsoft <vahidsotudeh@gmail.com>
2016-05-10 10:10:41 +0430

    servlet addded

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,9 @@
  * Created by ${Microsoft} on 10/05/2016.
  */
 public class Main {
+    public static String s="sd";
+    public static void main(String[] args) {
+
+
+    }
 }
--- src/index.java
+++ src/index.java
@@ -1,5 +1,22 @@
 /**
  * Created by ${Microsoft} on 10/05/2016.
  */
-public class index extends  {
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class index extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+
+        // Actual logic goes here.
+        PrintWriter out = response.getWriter();
+        out.println("<h1>" + Main.s + "</h1>");
+    }
 }
